ee73762eaa35f1cbe1cad6dcc02e6b0fa47732bb
unknown <tommy.mortymer@gmail.com>
2021-04-13 18:15:48 +0300

    projekti aloitettu

---
+++ 2021 Kevät Tiistai Unity 3D Peli/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float juoksuNopeus = 3.5f;
+    public float hiirenNopeus = 3f;
+    public float hyppyNopeus = 100f;
+    public float painovoima = 10f;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Joka fysikka frame mun mielestä
+    void FixedUpdate()
+    {
+
+
+    }
+
+
+}
